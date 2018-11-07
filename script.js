0ba0de21df0974d2cae7c5197ee50e57dc546e3a
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-06 23:20:55 -0500

    update2

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-  let z = 1;
+// $(document).ready(function(){
+//   let z = 1;
 
-  $("button").on('click', function() {
-    z++;
-    alert(z);
-  });
+//   $("button").on('click', function() {
+//     z++;
+//     alert(z);
+//   });
 
 
-$("button").off("click");
-});
+// $("button").off("click");
+// });
 
 // let a = 1;
 // console.log(window.a === 1);
@@ -90,21 +90,134 @@ $("button").off("click");
 
 // }('hello', 'world'));
 
-let a = 1;
+// let a = 1;
 
-console.log(this);
-console.log(this === window);
+// console.log(this);
+// console.log(this === window);
 
-console.log(window.a);
-console.log(this.a);
-console.log(a);
+// console.log(window.a);
+// console.log(this.a);
+// console.log(a);
 
-function foo(){
-  console.log(this);
-}
-foo();
+// function foo(){
+//   console.log(this);
+// }
+// foo();
 
-function doStuff (a, b) {
+// function doStuff (a, b) {
+//   console.log(arguments);
+//   // consollog arguments - shows them in detail in the console. very cool
+// }
+// doStuff('hi', 2, 3+5);
+
+// let obj = {
+//   value: 0,
+//   increment: function() { this.value++; }
+// };
+
+// obj.increment();
+
+//javascript will set the <this> parameter to the object where the method function was invoked on.
+
+// let value = 500;
+// let obj = {
+//   value: 0;
+//   increment: function() {
+//     this.value++;
+
+//     let innerFunction = function() {
+//       console.log(this.value);
+//     }
+//     innerFunction();
+//     //innerFunction() this is set to the global object.
+//   }
+// }
+// obj.increment();
+
+// let value = 500;
+// let obj = {
+//   value: 0,
+//   increment: function() {
+//     let that = this;
+//     that.value++;
+
+//     let innerFunction = function() {
+//       console.log(that.value);
+//     }
+//     innerFunction();
+//   }
+// }
+// obj.increment();
+
+// let Cheese = function(typeOfCheese) {
+//   let cheeseType = typeOfCheese;
+//   console.log(cheeseType);
+//   return cheeseType;
+// }
+// cheddar = new Cheese("gouda");
+
+// let obj = {
+//   foo: function(a, b, c,) {
+//     console.log( arguments );
+//     console.log( this );
+//   }
+// };
+
+// obj.foo(1,2,3);
+
+// obj.foo.apply(window, [1,2,3]);
+
+// let fer = {name: 'Fer', coder: true};
+// let harry = {name: 'Harry', coder: true};
+
+// let hi = function(){
+//   console.log('whatsup, ' + this.name);
+// };
+
+// let bye = function(){
+//   console.log('laters, ' + this.name);
+// };
+
+// hi();
+// hi.call(fer);
+// bye.call(harry);
+
+// let fer   = {name: 'Fer',   coder: true, nationality: 'Mexican'};
+// let harry = {name: 'Harry', coder: true, nationality: 'Taiwanese'};
+
+// let update = function(name, coder, nationality){
+//   this.name = name;
+//   this.coder = coder;
+//   this.nationality = nationality;
+// };
+
+// update.call(fer, 'Fer', true, 'Spanish');
+// // .call with the argument list for 2nd parameter
+// update.apply(harry, ['Harry', true, 'Canadian']);
+// // .apply with the array list for 2nd parameter
+
+var addGrades_CALL = function(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10) {
   console.log(arguments);
+  var sum = 0;
+  for (var i=0; i < arguments.length; i++) {
+      sum += arguments[i];
+  }
+  return sum;
 }
-doStuff('hi', 2, 3+5);
+
+addGrades_CALL.call(null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+var grades1 = [1,2,3,4,5];
+var grades2 = [1,2,3,4,5,6,7,8,9,10];
+
+var addGrades_APPLY = function() {
+    console.log(arguments);
+    var sum = 0;
+    for (var i=0; i < arguments[1].length; i++) {
+        sum += arguments[1][i];
+    }
+    return sum;
+}
+
+addGrades_APPLY(null, grades1);
+addGrades_APPLY(null, grades2);
